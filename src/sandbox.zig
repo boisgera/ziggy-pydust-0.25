ca68bc828cb0be1bfa2331a7d1555e84f7c51b77
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-05-25 20:33:23 +0200

    Build broken by `norm`

--- src/sandbox.zig
+++ src/sandbox.zig
@@ -12,9 +12,9 @@ pub fn origin() Vector {
     return Vector{ .x = 0.0, .y = 0.0 };
 }
 
-// pub fn norm(args: struct { v: Vector }) f64 {
-//     return std.math.sqrt(args.v.x * args.v.x + args.v.y * args.v.y);
-// }
+pub fn norm(args: struct { v: Vector }) f64 {
+    return std.math.sqrt(args.v.x * args.v.x + args.v.y * args.v.y);
+}
 
 comptime {
     py.rootmodule(root);
